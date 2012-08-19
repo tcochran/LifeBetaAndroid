bfacb8a4b014ec02d3b604f4720a6b8e4c3be0a5
Timothy Cochran <timothy.cochran@gmail.com>
2012-08-19 14:59:14 -0300

    Initial checkin, skeleton android app

---
+++ src/com/alala/mylife/Task.java
@@ -0,0 +1,34 @@
+package com.alala.mylife;
+
+public class Task {
+
+	private String name;
+	private int id;
+	private String description;
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+}
---
+++ src/com/alala/mylife/TaskWebService.java
@@ -0,0 +1,12 @@
+package com.alala.mylife;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskWebService {
+
+	public List<Task> getTasks()
+	{
+		return new ArrayList<Task>();
+	}
+}
